731e4c7ab6f091f0f53cf32b10528d443eb23697
soniigx <soniagquin@gmail.com>
2022-11-08 00:49:36 -0500

    Took out the prompt on js

--- src/app.js
+++ src/app.js
@@ -7,16 +7,3 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
-
-/* let, if, else, alert, prompt */
-alert("Hi! Welcome to my website!\nYou get a virtual cookie🍪");
-
-var name = prompt("What is your name?");
-if (name == null || name == "") {
-  txt = "I see that you have not responded... Welcome then Anonymous";
-} else {
-  txt = "Hello, " + name + "!";
-}
-alert(txt);
-
-/* My Event is on my blog page */
